4c7c64734e668221fe7cdc02f3c41d2c0750d9af
Leandro Henrique <leandrohamorim@outlook.com>
2021-07-24 02:54:43 -0300

    Developed get person by id

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -7,36 +7,43 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-	
+
 	private PersonService personService;
-	
+
 	@Autowired
 	public PersonController(PersonService personService) {
 		this.personService = personService;
 	}
 
-
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
-		return personService.createPerson(personDTO);	
-		}
-	
-	  @GetMapping
-	    public List<PersonDTO> listAll() {
-	        return personService.listAll();
-	    }
+		return personService.createPerson(personDTO);
+	}
+
+	@GetMapping
+	public List<PersonDTO> listAll() {
+		return personService.listAll();
+	}
+
+	@GetMapping("/{id}")
+	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+		return personService.findById(id);
+	}
 
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,5 +1,7 @@
 package one.digitalinnovation.personapi.service;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,38 +10,41 @@ import org.springframework.stereotype.Service;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 
 @Service
 public class PersonService {
-	
+
 	private PersonRepository personRepository;
-	
+
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
-	
+
 	@Autowired
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
 	}
-	
+
 	public MessageResponseDTO createPerson(PersonDTO personDTO) {
-		
+
 		Person personToSave = personMapper.toModel(personDTO);
-		
+
 		Person savedPerson = personRepository.save(personToSave);
-		return MessageResponseDTO
-				.builder()
-				.message("Created person with ID: " + savedPerson.getId())
-				.build();
+		return MessageResponseDTO.builder().message("Created person with ID: " + savedPerson.getId()).build();
 	}
 
-	 public List<PersonDTO> listAll() {
-	        List<Person> people = personRepository.findAll();
-	        return people.stream()
-	                .map(personMapper::toDTO)
-	                .collect(Collectors.toList());
-	    }
+	public List<PersonDTO> listAll() {
+		List<Person> people = personRepository.findAll();
+		return people.stream().map(personMapper::toDTO).collect(Collectors.toList());
+	}
 
-	
+	public PersonDTO findById(Long id) throws PersonNotFoundException {
+		// Optional<Person> optionalPerson = personRepository.findById(id);
+		Person person = personRepository
+				.findById(id)
+				.orElseThrow(() -> new PersonNotFoundException(id));
+		return personMapper.toDTO(person);
+	}
+
 }
